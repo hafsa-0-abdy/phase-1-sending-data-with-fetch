6377dec26f68681c85cd5b03da4e37a7a6f73ae5
Habsa Abdirizack <habsa.abdirizack@student.moringaschool.com>
2024-10-21 18:04:02 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(name,email){
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'    
+        },
+        body: JSON.stringify({name: name, email: email}),
+    })
+    .then((resp) => resp.Json())
+    .then((data) => {
+        document.querySelector('body').append(data.id)
+    })
+    .catch((err) => {
+        alert('Error:Wrong')
+        document.querySelector('body').append(err.message)
+    })
+}
